86635013d0814ab42a34377a20103b164ae1632e
Karol Dominiak <gatoke2@gmail.com>
2021-03-15 00:27:37 +0100

    chore: Logging command execution time

--- src/main/java/com/github/gatoke/offers/application/shared/CommandBus.java
+++ src/main/java/com/github/gatoke/offers/application/shared/CommandBus.java
@@ -3,23 +3,36 @@ package com.github.gatoke.offers.application.shared;
 import com.github.gatoke.offers.domain.shared.DomainEvent;
 import com.github.gatoke.offers.domain.shared.EventPublisher;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
 
+import static java.lang.System.currentTimeMillis;
+
 @Component
 @RequiredArgsConstructor
+@Slf4j(topic = "com.github.gatoke.offers.commandbus")
 public class CommandBus {
 
     private final EventPublisher eventPublisher;
     private final CommandsRegistry commandsRegistry;
+    private final TransactionTemplate transactionTemplate;
 
-    @Transactional
-    public <T extends Command> void execute(final Command command) {
+    public void execute(final Command command) {
         final CommandHandler<Command> commandHandler = commandsRegistry.get(command.getClass());
 
-        final List<? extends DomainEvent> resultEvents = commandHandler.execute(command);
-        resultEvents.forEach(eventPublisher::publish);
+        final long start = currentTimeMillis();
+        try {
+            transactionTemplate.executeWithoutResult(status -> {
+                final List<? extends DomainEvent> resultEvents = commandHandler.execute(command);
+                resultEvents.forEach(eventPublisher::publish);
+            });
+        } finally {
+            final long end = currentTimeMillis();
+            final long timeElapsed = end - start;
+            log.debug("Command handler {} finished in {}ms", commandHandler.getClass().getSimpleName(), timeElapsed);
+        }
     }
 }
