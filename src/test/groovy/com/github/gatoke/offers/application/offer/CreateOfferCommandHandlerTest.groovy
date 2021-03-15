1bd65be30934f9548fdddf59e7d48824b41b78c7
Karol Dominiak <gatoke2@gmail.com>
2021-03-15 19:04:50 +0100

    test: fix failing tests

---
+++ src/test/groovy/com/github/gatoke/offers/application/offer/CreateOfferCommandHandlerTest.groovy
@@ -0,0 +1,55 @@
+package com.github.gatoke.offers.application.offer
+
+import com.github.gatoke.offers.application.offer.command.CreateOfferCommand
+import com.github.gatoke.offers.domain.offer.Offer
+import com.github.gatoke.offers.domain.offer.OfferRepository
+import com.github.gatoke.offers.domain.offer.vo.OfferId
+import com.github.gatoke.offers.domain.shared.EventType
+import com.github.gatoke.offers.domain.user.UserRepository
+import com.github.gatoke.offers.domain.user.vo.UserId
+import spock.lang.Specification
+
+import java.time.OffsetDateTime
+
+class CreateOfferCommandHandlerTest extends Specification {
+
+    def "should create offer"() {
+        given:
+        def offerRepository = new TestOfferRepository()
+        def userRepository = Mock(UserRepository)
+        def commandHandler = new CreateOfferCommandHandler(offerRepository, userRepository)
+        and:
+        userRepository.doesNotExist(UserId.of(1234L)) >> false
+
+        when:
+        def result = commandHandler.execute(
+                new CreateOfferCommand(1234L, 'iPhone7', 'Selling iPhone 7 64GB, good quality')
+        )
+
+        then:
+        result.size() == 1
+        and:
+        result.first().type == EventType.OFFER_CREATED
+    }
+
+    private class TestOfferRepository implements OfferRepository {
+
+        private List<Offer> offers = new ArrayList<>()
+
+        @Override
+        Offer save(Offer offer) {
+            offers.add(offer)
+            return offer
+        }
+
+        @Override
+        Offer findOrFail(OfferId offerId) {
+            return null
+        }
+
+        @Override
+        List<Offer> findPublishedOffersCreatedBefore(OffsetDateTime dateTime) {
+            return null
+        }
+    }
+}
---
+++ src/test/groovy/com/github/gatoke/offers/application/user/RegisterUserCommandHandlerTest.groovy
@@ -0,0 +1,51 @@
+package com.github.gatoke.offers.application.user
+
+import com.github.gatoke.offers.application.user.command.RegisterUserCommand
+import com.github.gatoke.offers.domain.shared.EventType
+import com.github.gatoke.offers.domain.user.User
+import com.github.gatoke.offers.domain.user.UserRepository
+import com.github.gatoke.offers.domain.user.vo.UserId
+import spock.lang.Specification
+
+class RegisterUserCommandHandlerTest extends Specification {
+
+    def "should register user"() {
+        given:
+        def userRepositoryMock = new TestUserRepository()
+        def commandHandler = new RegisterUserCommandHandler(userRepositoryMock)
+        and:
+        userRepositoryMock.exists(UserId.of(1234L)) >> false
+
+        when:
+        def result = commandHandler.execute(
+                new RegisterUserCommand(1234L, 'John', 'Kowalski', 'john@kowalski.pl')
+        )
+
+        then:
+        result.size() == 1
+        and:
+        result.first().type == EventType.USER_REGISTERED
+    }
+
+
+    private class TestUserRepository implements UserRepository {
+
+        private List<User> users = new ArrayList<>()
+
+        @Override
+        User save(User user) {
+            users.add(user)
+            return user
+        }
+
+        @Override
+        boolean doesNotExist(UserId userId) {
+            return false
+        }
+
+        @Override
+        boolean exists(UserId userId) {
+            return false
+        }
+    }
+}
